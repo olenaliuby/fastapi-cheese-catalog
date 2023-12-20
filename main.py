326b6dd982e397b1521bc0d1bd6597ba86e013f3
Olena Liubynetska <olena.liuby@gmail.com>
2023-12-20 17:53:14 +0000

    feat: Add endpoint for creating cheese types

--- main.py
+++ main.py
@@ -25,3 +25,11 @@ def root() -> dict:
 @app.get("/cheese_types/", response_model=list[schemas.CheeseType])
 def read_cheese_types(db: Session = Depends(get_db)):
     return crud.get_all_cheese_types(db=db)
+
+
+@app.post("/cheese_types/", response_model=schemas.CheeseType)
+def create_cheese_type(
+        cheese_type: schemas.CheeseTypeCreate,
+        db: Session = Depends(get_db)
+):
+    return crud.create_cheese_type(db=db, cheese_type=cheese_type)
