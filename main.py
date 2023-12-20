58878ec3848a76cd8dfd70dc86d24951f6dfae94
Olena Liubynetska <olena.liuby@gmail.com>
2023-12-20 18:09:41 +0000

    feat: Add endpoints for cheese

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 import crud
 import schemas
 from db.engine import SessionLocal
+from db.models import PackagingType
 
 app = FastAPI()
 
@@ -41,3 +42,29 @@ def create_cheese_type(
         )
 
     return crud.create_cheese_type(db=db, cheese_type=cheese_type)
+
+
+@app.get("/cheese/", response_model=list[schemas.Cheese])
+def read_cheese(
+        packaging_type: PackagingType | None = None,
+        cheese_type: str | None = None,
+        db: Session = Depends(get_db),
+):
+    return crud.get_cheese_list(
+        db=db, packaging_type=packaging_type, cheese_type=cheese_type
+    )
+
+
+@app.get("/cheese/{cheese_id}/", response_model=schemas.Cheese)
+def read_single_cheese(cheese_id: int, db: Session = Depends(get_db)):
+    db_cheese = crud.get_cheese(db=db, cheese_id=cheese_id)
+
+    if db_cheese is None:
+        raise HTTPException(status_code=404, detail="Cheese not found")
+
+    return db_cheese
+
+
+@app.post("/cheese/", response_model=schemas.Cheese)
+def create_cheese(cheese: schemas.CheeseCreate, db: Session = Depends(get_db)):
+    return crud.create_cheese(db=db, cheese=cheese)
