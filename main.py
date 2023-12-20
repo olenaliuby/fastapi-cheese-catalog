3209630bd40256b9ca404c9247388f4b5acd3f64
Olena Liubynetska <olena.liuby@gmail.com>
2023-12-20 17:43:01 +0000

    feat: Create / and /cheese_types/ endpoints

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+import schemas
+from db.engine import SessionLocal
+
+app = FastAPI()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def root() -> dict:
+    return {"message": "Hello World"}
+
+
+@app.get("/cheese_types/", response_model=list[schemas.CheeseType])
+def read_cheese_types(db: Session = Depends(get_db)):
+    return crud.get_all_cheese_types(db=db)
